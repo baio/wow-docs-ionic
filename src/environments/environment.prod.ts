337a0f767effb4da5b046fab0383f74934bfe5f7
maxxx80 <maxjvc11+1@gmail.com>
2022-09-15 10:17:18 +0300

    product env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  social: {
+    yandex: {
+      clientId: '7489e5aae33b4568bf21cb434060df4d',
+      redirectUrl: 'pw.perimeter.wowdocs://login',
+      scope:
+        'cloud_api:disk.app_folder cloud_api:disk.read cloud_api:disk.write cloud_api:disk.info',
+    },
+  },
 };
