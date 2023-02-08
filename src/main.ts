f8a66be77bf3092b1152e96517d5994b3e64d80c
maxxx80 <maxjvc11+1@gmail.com>
2023-02-08 11:17:57 +0500

    cleanup web init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { defineCustomElements as jeepSqlite} from 'jeep-sqlite/loader';
-import { defineCustomElements } from '@ionic/pwa-elements/loader';
-
+import { defineCustomElements as jeepSqlite } from 'jeep-sqlite/loader';
+import { defineCustomElements as pwaElements } from '@ionic/pwa-elements/loader';
+import { Capacitor } from '@capacitor/core';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -11,8 +11,23 @@ if (environment.production) {
 }
 // TODO : Disable for mobile platforms
 jeepSqlite(window);
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
 
-defineCustomElements(window);
+const platform = Capacitor.getPlatform();
+if (platform === 'web') {
+  // Web platform
+  // required for toast component in Browser
+  pwaElements(window);
 
+  // required for jeep-sqlite Stencil component
+  // to use a SQLite database in Browser
+  jeepSqlite(window);
+
+  window.addEventListener('DOMContentLoaded', async () => {
+    const jeepEl = document.createElement('jeep-sqlite');
+    document.body.appendChild(jeepEl);
+  });
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.log(err));
